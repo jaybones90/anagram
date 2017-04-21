7c9745cd09c2566b0a5bab617aa27ee2f2d8fbfb
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-04-21 09:48:46 -0700

    add in second test to see if two words are anagrams regardless of case and it passes

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,7 @@
 class String
   define_method(:anagram) do |word1,word2|
-    word1_array = word1.split("")
-    word2_array = word2.split("")
+    word1_array = word1.upcase.split("")
+    word2_array = word2.upcase.split("")
     new_array = []
     answer = ""
 
@@ -9,12 +9,12 @@ class String
       word1_array.include?(letter)
       if true
         new_array.push(letter)
-
-      else print "false"
       end
     end
+
     if new_array.length == word1_array.length
     answer = "this is an anagram"
     end
+
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,8 +2,12 @@ require('rspec')
 require('anagram') #requires lib file that is .rb
 
 describe() do
-  it("will check to see if two words are anagrams") do #doesnt matter english explination
-    expect("".anagram("ruby","bury")).to(eq("this is an anagram"))
-
+  it("will check to see if two words are anagrams") do
+    expect("".anagram("canoe","ocean")).to(eq("this is an anagram"))
   end
+
+  it("will check to see if two words are anagrams regardeless of case") do
+    expect("".anagram("Canoe","oCean")).to(eq("this is an anagram"))
+  end
+
 end
